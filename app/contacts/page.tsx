7a68e54bffe26cbfcfac9350c0c2f1875e9a5dea
Mario Lazzari <mario.lazzari@gmail.com>
2024-11-16 23:13:28 +0100

    feat: contacts page

---
+++ app/contacts/page.tsx
@@ -0,0 +1,34 @@
+import { Contact } from "@/components/Contact";
+import { ContactProps } from "@/components/Contact/ContactProps";
+
+function ContactsPage() {
+  const contacts: ContactProps[] = [
+    {
+      title: "Maria Filippini Lazzari",
+      description: "Autrice teatrale bresciana",
+      url: "https://mariafilippini.it",
+      mail: "mariafilippinilazzari@gmail.com",
+      imageSrc: "/images/logo.jpg",
+    },
+    {
+      title: "Mario Lazzari",
+      description: "Senior full stack developer",
+      url: "https://mariolazzari.it",
+      mail: "mario.lazzari@gmail.com",
+      imageSrc: "/images/logoMario.jpg",
+    },
+  ];
+
+  return (
+    <>
+      <h2 className="text-4xl mb-12 text-center">Contatti</h2>
+      <div className="flex justify-center gap-8 flex-wrap">
+        {contacts.map(contact => (
+          <Contact key={contact.url} {...contact} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default ContactsPage;
---
+++ components/Contact/index.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+import { ContactProps } from "./ContactProps";
+import { Mail, LinkIcon } from "lucide-react";
+
+export function Contact({
+  title,
+  description,
+  url,
+  imageSrc,
+  mail,
+}: ContactProps) {
+  return (
+    <div className="w-[300px] h-[400px] p-4 border border-primary rounded-xl">
+      <Image
+        className="rounded-full mx-auto mb- shadow-md"
+        src={imageSrc}
+        alt={title}
+        width={192}
+        height={192}
+      />
+      <h2 className="text-center font-bold text-2xl">{title}</h2>
+      <h3 className="text-center text-lg mb-12">{description}</h3>
+
+      <Link
+        className="flex items-center gap-2 mb-1"
+        href={`mailto://${mail}`}
+        target="_blank"
+      >
+        <Mail size={20} /> {mail}
+      </Link>
+      <Link className="flex items-center gap-2" href={url} target="_blank">
+        <LinkIcon size={20} /> {url}
+      </Link>
+    </div>
+  );
+}
